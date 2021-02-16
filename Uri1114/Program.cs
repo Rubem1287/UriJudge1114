c12c58d3e078dca93e6ef798fd979f828ff6dad7
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 16:14:54 -0300

    Add files via upload

---
+++ Uri1114/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Uri1114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha;
+
+            senha = int.Parse(Console.ReadLine());
+
+            while(senha != 2002)
+            {
+                Console.WriteLine("Senha inválida");
+
+                senha = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
